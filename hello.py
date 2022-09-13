7030abf227db6fe590f19437bbcf8c2e6d90a533
jhelmke83 <jhelmke83@charlottesvilleschools.org>
2022-09-13 09:30:50 -0400

    hello.py finished

---
+++ hello.py
@@ -0,0 +1,25 @@
+# SPDX-FileCopyrightText: 2018 Kattni Rembor for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+
+"""CircuitPython Essentials Internal RGB LED rainbow example"""
+import time
+import board
+from rainbowio import colorwheel
+
+if hasattr(board, "APA102_SCK"):
+    import adafruit_dotstar
+
+    led = adafruit_dotstar.DotStar(board.APA102_SCK, board.APA102_MOSI, 1)
+else:
+    import neopixel
+
+    led = neopixel.NeoPixel(board.NEOPIXEL, 1)
+
+led.brightness = 0.1
+
+i = 0
+while True:
+    i = (i + 1) % 256  # run from 0 to 255
+    led.fill(colorwheel(i))
+    time.sleep(0.01)
